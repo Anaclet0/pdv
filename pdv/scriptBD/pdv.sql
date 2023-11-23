3d3b0074f5031420ea2870a3703833cfe6b903bd
Anacleto-_- <151063513+Anaclet0@users.noreply.github.com>
2023-11-23 19:57:57 -0300

    banco de dados

--- pdv/scriptBD/pdv.sql
+++ pdv/scriptBD/pdv.sql
@@ -1,49 +1,46 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
---
--- Host: localhost    Database: pdv
--- ------------------------------------------------------
--- Server version	8.0.32
+create database pdv;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `loja`
---
+-- Tabela para a entidade Loja
+CREATE TABLE loja (
+    id_loja INT PRIMARY KEY auto_increment,
+	nome VARCHAR(255)
+    
+);
 
-DROP TABLE IF EXISTS `loja`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `loja` (
-  `id` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Tabela para a entidade Produto
+CREATE TABLE produto (
+    id_produto INT PRIMARY KEY auto_increment,
+    nome VARCHAR(255),
+    preco DECIMAL(10, 2)
+);
 
---
--- Dumping data for table `loja`
---
+-- Tabela para a entidade Vendedor
+CREATE TABLE vendedor (
+    id_vendedor INT PRIMARY KEY auto_increment,
+    nome VARCHAR(255)
+    
+);
 
-LOCK TABLES `loja` WRITE;
-/*!40000 ALTER TABLE `loja` DISABLE KEYS */;
-/*!40000 ALTER TABLE `loja` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- Tabela para a entidade Venda
+CREATE TABLE venda (
+    id_venda INT PRIMARY KEY auto_increment,
+    data_venda DATE,
+    id_loja INT,
+    id_vendedor INT,
+    FOREIGN KEY (id_loja) REFERENCES Loja(id_loja),
+    FOREIGN KEY (id_vendedor) REFERENCES Vendedor(id_vendedor)
+);
 
--- Dump completed on 2023-11-07 20:56:23
+-- Tabela para a entidade Item
+CREATE TABLE item (
+    id_item INT PRIMARY KEY auto_increment,
+    quantidade INT,
+    id_produto INT,
+    id_venda INT,
+    FOREIGN KEY (id_produto) REFERENCES Produto(id_produto),
+    FOREIGN KEY (id_venda) REFERENCES Venda(id_venda)
+);
+
+
